f8b0a11d87de4b92b6fcdf7045d9ac4b389a6dbf
André <dmastergame74@gmail.com>
2023-10-21 16:59:11 -0300

    update: import chromium

--- scrapper.ts
+++ scrapper.ts
@@ -1,8 +1,17 @@
+import chromium from '@sparticuz/chromium-min';
 import puppeteer from 'puppeteer';
 
 export async function scrapeEvents(url: string, eventContainerSelector: string, titleSelector: string, dateSelector: string, locationSelector: string, imageSelector: string) {
   try {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(
+        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+      ),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
     const page = await browser.newPage();
 
     await page.goto(url);
