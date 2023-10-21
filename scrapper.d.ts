06b1f8f8c560fec84f60552d65f5a381908cb814
André <dmastergame74@gmail.com>
2023-10-21 15:49:51 -0300

    refactor: clean code and move from js to ts

--- scrapper.d.ts
+++
@@ -1,5 +0,0 @@
-// scrapper.d.ts
-declare module 'scrapper' {
-    export function scrapeEvents(url: string, eventContainerSelector: string, titleSelector: string, dateSelector: string, locationSelector: string, imageSelector: string): Promise<any[]>;
-  }
-  
---
+++ scrapper.ts
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer';
+
+export async function scrapeEvents(url: string, eventContainerSelector: string, titleSelector: string, dateSelector: string, locationSelector: string, imageSelector: string) {
+  try {
+    const browser = await puppeteer.launch({ headless: 'new' });
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const eventsContainers = await page.$$(eventContainerSelector);
+
+    const events = [];
+
+    for (const event of eventsContainers) {
+      const title = await event.$eval(titleSelector, (item) => item ? item.textContent : '');
+      const dateContainer = await event.$(dateSelector);
+      const datesText = dateContainer ? await page.evaluate((element) => element.textContent, dateContainer) : '';
+      const dates = datesText ? datesText.match(/\d{1,2} [A-Za-z]{3}/g) : null;
+
+      const location = await event.$eval(locationSelector, (item) => item ? item.textContent : '');
+      const image = await event.$eval(imageSelector, (item) => item ? item.getAttribute('src') : '');
+
+      events.push({
+        title,
+        dates,
+        location,
+        image
+      });
+    }
+
+    await browser.close();
+
+    return events;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
